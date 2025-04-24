53157818ba10070874f0caf4d117a5e8dc25f4b2
codekarimi <ckarimi676@gmail.com>
2025-04-24 18:17:29 +0300

    Clerk Auth

---
+++ yoom/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export const metadata: Metadata = {
+  title: "YOOM✔",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+     <ClerkProvider>
+     <body
+        className={`${inter.className} bg-dark-2`}
+      >
+        {children}
+      </body>
+     </ClerkProvider>
+    </html>
+  );
+}
---
+++ yoom/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link'
+import React from 'react'
+import MobileNav from '@/components/MobileNav';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+
+const navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full-bg-dark-1 px-6 py-4 lg:px-10 ">
+      <Link href={"/"} className="flex items-centre gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Yoom logo"
+          width={30}
+          height={30}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white  max-sm:hidden">Yoom</p>
+      </Link>
+
+
+      <div className="flex-between gap-5">
+
+            <SignedIn>
+                    <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+
+        <MobileNav/>
+      </div>
+    </nav>
+  );
+}
+
+export default navbar
